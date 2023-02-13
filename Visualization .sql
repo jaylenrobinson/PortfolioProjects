e2e920720af21db45a228c0e5c9d6142aed05813
jaylenrobinson <123053501+jaylenrobinson@users.noreply.github.com>
2023-02-13 15:06:32 -0600

    Update Visualization .sql

--- Visualization .sql
+++ Visualization .sql
@@ -1,11 +1,11 @@
 
---   Rides per month by member type 
+--   Ride month by member type 
 
    SELECT member_type, ride_month, count(*) AS rides_per_month
    FROM `capstone-project2023.cyclistic_trip_data.combined_tripdata`
    GROUP BY member_type, ride_month
 
---  Rides per day by member type
+--  Ride day by member type
 
    SELECT member_type, ride_day, count(*) AS rides_per_day
    FROM `capstone-project2023.cyclistic_trip_data.combined_tripdata`
